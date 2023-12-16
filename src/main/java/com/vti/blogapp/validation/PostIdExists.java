f8ec82ef84f56cafc6e0bc07eb4cd6719c93992c
MTduong98 <maitheduong1998@gmail.com>
2023-12-16 19:20:25 +0900

    [Add] Custom annotation PostIdExists

---
+++ src/main/java/com/vti/blogapp/validation/PostIdExists.java
@@ -0,0 +1,21 @@
+package com.vti.blogapp.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Constraint(
+        validatedBy = PostIdExistsValidator.class
+)
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface PostIdExists {
+    String message() default "{The post does not exist.}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}

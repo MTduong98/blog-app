f45e3eb4912d6a7f889240c06e9168603ce9d432
MTduong98 <maitheduong1998@gmail.com>
2023-12-16 18:50:47 +0900

    [Add] Validate CommentCreateForm & CommentUpdateForm

--- src/main/java/com/vti/blogapp/controller/CommentController.java
+++ src/main/java/com/vti/blogapp/controller/CommentController.java
@@ -5,11 +5,14 @@ import com.vti.blogapp.form.CommentCreateForm;
 import com.vti.blogapp.form.CommentFilterForm;
 import com.vti.blogapp.form.CommentUpdateForm;
 import com.vti.blogapp.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @AllArgsConstructor
 public class CommentController {
@@ -26,13 +29,13 @@ public class CommentController {
 
     @PostMapping("/api/v1/posts/{postId}/comments")
 
-    public CommentDto create(@RequestBody CommentCreateForm form,
+    public CommentDto create(@RequestBody @Valid CommentCreateForm form,
                              @PathVariable("postId") Long postId) {
         return commentService.create(form, postId);
     }
     @PutMapping("/api/v1/comment/{id}")
     public CommentDto update(
-            @RequestBody CommentUpdateForm form,
+            @RequestBody @Valid CommentUpdateForm form,
             @PathVariable("id") Long id
     ){
         return commentService.update(form,id);

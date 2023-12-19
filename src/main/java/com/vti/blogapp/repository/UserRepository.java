248657bd13b26277f34829b9bec6e6d57eb99b41
MTduong98 <maitheduong1998@gmail.com>
2023-12-19 21:58:54 +0900

    [Add] Login using database

--- src/main/java/com/vti/blogapp/repository/UserRepository.java
+++ src/main/java/com/vti/blogapp/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.vti.blogapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String name);
+
 
 }
--- src/main/java/com/vti/blogapp/service/UserServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/UserServiceImpl.java
@@ -6,14 +6,20 @@ import com.vti.blogapp.form.UserCreateForm;
 import com.vti.blogapp.mapper.UserMapper;
 import com.vti.blogapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDto create(UserCreateForm form) {
         var user = UserMapper.map(form);
@@ -23,4 +29,16 @@ public class UserServiceImpl implements UserService{
         return UserMapper.map(savedUser);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(),
+                user.getPassword(),
+                AuthorityUtils.NO_AUTHORITIES
+        );
+    }
 }

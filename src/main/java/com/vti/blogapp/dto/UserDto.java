c500dd1fc87fe059981da7e6ee011cdf0017a552
MTduong98 <maitheduong1998@gmail.com>
2023-12-19 21:36:32 +0900

    [Add] Encode user password

---
+++ src/main/java/com/vti/blogapp/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.vti.blogapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
